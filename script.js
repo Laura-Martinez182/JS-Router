e14c9e549c5b4a5093ce40f05a084b3bb003d77b
DianaOlanoU <1005967885@u.icesi.edu.co>
2022-02-12 18:10:22 -0500

    Second part of the tutorial

--- script.js
+++ script.js
@@ -37,3 +37,38 @@ function route (path, template) {
         return;
     };
 };
+
+function template (name, templateFunction) {
+    return templates[name] = templateFunction;
+};
+
+template('home', function(){
+    home();
+});
+
+template('about', function(){
+    about();
+});
+
+route('/', 'home');
+
+route('/about', 'about');
+
+function resolveRoute(route) {
+    try {
+        return routes[route];
+    } catch (e) {
+        throw new Error(`Route ${route} not found`);
+    };
+};
+
+function router(evt) {
+    let url = window.location.hash.slice(1) || '/';
+    let route = resolveRoute(url);
+
+    route();
+};
+
+window.addEventListener('load', router);
+
+window.addEventListener('hashchange', router);
